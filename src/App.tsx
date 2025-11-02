93689eb65dfcd1e3436a75aff2633fb60d8d30b5
maelseewal <maelseewald@gmail.com>
2025-11-02 16:33:43 +0100

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import AusweisPage from "./pages/ausweispage";
 
 function App() {
-  return <AusweisPage />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<AusweisPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
